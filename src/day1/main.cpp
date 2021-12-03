89c2d9472a09691baefb030a8b362259eb18da52
Miguel Moreno <miguel.moreno@gmx.de>
2021-12-03 17:45:34 +0100

    Day 1 part 2

--- src/day1/main.cpp
+++ src/day1/main.cpp
@@ -1,10 +1,13 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
-int main(){
+
+void partOne()
+{
     ifstream myfile("input");
 
     int depth = 0;
@@ -24,6 +27,44 @@ int main(){
         lastDepth = depth;
     }
     
-    cout << "Number increases: " << incCounter << endl;
+    cout << "Number increases part one: " << incCounter << endl;
+}
+
+void partTwo()
+{
+    ifstream myfile("input");
+
+    int window = 0;
+    int lastWindow = 0;
+    int incCounter = 0;
+    vector<int> depths;
+
+    std::string inputLine;
+    while(getline(myfile, inputLine))
+    {
+        stringstream converter(inputLine);
+        int depth = 0;
+        converter >> depth;
+
+        depths.push_back(depth);
+    }
+
+    for (int i = 2; i < depths.size(); i++)
+    {
+        int window = depths[i] + depths[i-1] + depths[i-2];
+
+        if (window > lastWindow && lastWindow != 0)
+                incCounter++;
+
+        lastWindow = window;
+    }
+    
+    cout << "Number increases part two: " << incCounter << endl;
+}
+
+
+int main(){
+    partOne();
+    partTwo();
     return 0;
 }
