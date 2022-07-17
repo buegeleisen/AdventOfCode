84d4bcd373f658f80fd7fa16d6f503c7ee3f5e9e
buegeleisen <miguel.moreno@gmx.de>
2022-07-17 22:36:01 +0200

    ka

---
+++ src/day10/main.cpp
@@ -0,0 +1,8 @@
+
+int roundCounter;
+int bracketCounter;
+int braceCounter;
+int angleCounter;
+
+// line parsen, für jedes '(' ein roundCounter++ und für jedes ')' ein roundCounter --, andere analog; ([]) okay, ([)] auch aber kaputt :/  
+// das geht also nicht, dann vll klammer auf von vorne entfernen und die zugehörige dann von hinte? 
